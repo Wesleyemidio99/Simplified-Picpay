afd150b51718ed9d450e0800561fac884bffa065
Wesleyemidio99 <wesleyemidio99@hotmail.com>
2025-05-30 09:51:29 -0300

    create a notification for when the transaction is done and AppConfig for RestTemplate

---
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final RestTemplate restTemplate;
+
+
+    public void sendNotification(User user, String messsage) throws Exception {
+        String email = user.getEmail();
+
+        NotificationDto notificationRequest = new NotificationDto(email, messsage);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            log.info("Erro ao enviar notificação para o usuário: {}", user.getId());
+            throw new Exception("Service de notificação indisponível.");
+        }
+    }
+}
